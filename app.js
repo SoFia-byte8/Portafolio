b650d7063eb63b02e77711d13f4d391f0aac7280
dcalderonalegre@gmail.com <dcalderonalegre@gmail.com>
2020-12-12 20:15:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+addEventListener('DOMcontentLoaded', () => {
+    const btn_main = document.querySelector('.btn_main')
+    if(btn_main) {
+        btn_main.addEventListener('click', () => {
+            const main_items = document.querySelector('.main_items')
+            main_items.classList.toggle('go')
+        })
+    }
+})
